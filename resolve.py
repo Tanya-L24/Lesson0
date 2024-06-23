225cd9f4b32f6251cff09ab5763fc561ce02b6ca
Tanya-L24 <tanyalebed@inbox.ru>
2024-06-23 22:34:33 +0300

    Add files via upload

---
+++ resolve.py
@@ -0,0 +1,25 @@
+a = 'soccer'
+print(len(a)) #Задача «Длина слова»
+
+first = int(2)
+second = int(5)
+summa = first+second
+diff = first-second
+print(summa, ';', diff) #Задача «Суммы и разности»
+
+first1 = 2
+second1 = 5
+third = 4
+mean = (first1 + second1 + third)/3
+print(mean) #Задача «Среднее арифметическое»
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ',' ,first_string) #Задача «Простые строчки»
+
+a = 3
+b = 5
+c = 6
+f = (((a * b) + (a * c))**3)/2
+print(f) #Задача «Сложная формула»
+
